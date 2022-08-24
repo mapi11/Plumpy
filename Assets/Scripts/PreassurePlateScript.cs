eecf89a8e073b876dcbbb5e7f2d0d7dba2e1b1b4
mapi <astramax2204@mail.ru>
2022-08-24 17:35:05 +0300

    1.4.2
    Pressure plate fixed and added anim

--- Assets/Scripts/PreassurePlateScript.cs
+++ Assets/Scripts/PreassurePlateScript.cs
@@ -6,37 +6,73 @@ public class PreassurePlateScript : MonoBehaviour
 {
     [SerializeField] private Vector3 _originalPos;
     [SerializeField] private GameObject _obj;
+    Animator _anim;
 
 
     private void Start()
     {
         _originalPos = transform.position;
+
+        _anim = GetComponent<Animator>();
     }
 
-    private void OnCollisionStay(Collision collision)
+    private void OnTriggerStay(Collider other)
     {
-        if (collision.transform.name == "PlateBlock")
+        if (other.transform.name == "PlateBlock")
         {
             Debug.Log("stay");
-
         }
     }
-    private void OnCollisionEnter(Collision collision)
+
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.transform.name == "PlateBlock")
+        if (other.transform.name == "PlateBlock")
         {
             Debug.Log("Enter");
             _obj.SetActive(true);
+
+            _anim.SetBool("PlateDown", true);
+            _anim.SetBool("PlateUp", false);
         }
     }
-    private void OnCollisionExit(Collision collision)
+
+    private void OnTriggerExit(Collider other)
     {
-        if (collision.transform.name == "PlateBlock")
+        if (other.transform.name == "PlateBlock")
         {
             Debug.Log("Exit");
             _obj.SetActive(false);
+
+            _anim.SetBool("PlateUp", true);
+            _anim.SetBool("PlateDown", false);
         }
     }
+
+
+    //private void OnCollisionStay(Collision collision)
+    //{
+    //    if (collision.transform.name == "PlateBlock")
+    //    {
+    //        Debug.Log("stay");
+
+    //    }
+    //}
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.transform.name == "PlateBlock")
+    //    {
+    //        Debug.Log("Enter");
+    //        _obj.SetActive(true);
+    //    }
+    //}
+    //private void OnCollisionExit(Collision collision)
+    //{
+    //    if (collision.transform.name == "PlateBlock")
+    //    {
+    //        Debug.Log("Exit");
+    //        _obj.SetActive(false);
+    //    }
+    //}
 
     private void Update()
     {
